11b0dc8acc628fa1ed86df2809d257db853612ff
marialukashenko <lukashenkomariia@gmail.com>
2025-09-23 18:22:31 +0300

    deleted form && added video

--- js/form.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("resume-file").addEventListener("change", function() {
-  const fileName = this.files.length > 0 ? this.files[0].name : "Файл не выбран";
-  document.getElementById("resume-file-name").textContent = fileName;
-});
-
-document.getElementById("motivation-file").addEventListener("change", function() {
-  const fileName = this.files.length > 0 ? this.files[0].name : "Файл не выбран";
-  document.getElementById("motivation-file-name").textContent = fileName;
-});
--- js/movetobuttons.js
+++ js/movetobuttons.js
@@ -1,6 +1,8 @@
 document.querySelectorAll('[data-target]').forEach(el => {
   el.addEventListener('click', function(e) {
     e.preventDefault(); // отменяем стандартный переход по ссылке
+    e.stopPropagation(); // 👈 добавляем эту строку
+    e.stopImmediatePropagation(); // 👈 и эту для надежности
     const targetId = this.getAttribute('data-target');
     const target = document.getElementById(targetId);
     if (target) {
